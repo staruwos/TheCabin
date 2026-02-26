6a7e44d25f70d0182f763dd60ce205fc9806cb7d
staruwos <juliotrasferetti@gmail.com>
2026-02-25 21:21:55 -0300

    (interfaces): add Interact function interface

--- Source/TheCabin/Public/CInteractInterface.h
+++ Source/TheCabin/Public/CInteractInterface.h
@@ -22,4 +22,6 @@ class THECABIN_API ICInteractInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Interaction")
+	void Interact(AActor* InteractingActor);
 };
